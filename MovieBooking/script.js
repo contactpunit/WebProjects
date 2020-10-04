92ed2d90e59365bfa272de5911b1fc26fb4ecc31
Punit Jain <contactpunitjain@gmail.com>
2020-10-04 12:21:12 +0530

    Added support for localStorage (browser storage). Each refresh retains old selections, movie and price

--- MovieBooking/script.js
+++ MovieBooking/script.js
@@ -17,10 +17,35 @@ class Movie {
         this.seatNumbers = [];
         this.movie.addEventListener('change', this.movieChangeHandler.bind(this))
         this.container.addEventListener('click', this.seatSelectHandler.bind(this));
+        this.initializeMovieData();
+    }
+
+    initializeMovieData() {
+        const moviePrice = localStorage.getItem('moviePrice');
+        const movieId = localStorage.getItem('movieId');
+        if (!movieId) {
+            localStorage.setItem('movieId', 0);
+            localStorage.setItem('moviePrice', +this.movie.value)
+        }
+        else {
+            const movieIndex = localStorage.getItem('movieId');
+            this.movie.selectedIndex = +movieIndex;
+            this.ticketPrice = localStorage.getItem('moviePrice');
+            this.seatNumbers = JSON.parse(localStorage.getItem('selectedSeatsIndex'));
+            if(this.seatNumbers) {
+                this.allSeats.forEach((seat, index) => {
+                    if(this.seatNumbers.includes(index)) {
+                        seat.classList.add('selected')
+                    }
+                });
+                this.updateCountAndPrice();
+            }
+        }
     }
 
     movieChangeHandler(event) {
         this.ticketPrice = +event.target.value;
+        this.updateMovieDetails(event);
         this.updateSelectedCount();
     }
 
@@ -32,18 +57,24 @@ class Movie {
         }
     }
 
+    updateMovieDetails(event) {
+        this.ticketPrice = +event.target.value;
+        localStorage.setItem('moviePrice', event.target.value);
+        localStorage.setItem('movieId', event.target.selectedIndex);
+    }
+
     updateSelectedCount() {
         this.selectedSeats = document.querySelectorAll('.row .seat.selected');
         this.seatNumbers = [...this.selectedSeats].map(seat => {
             return [...this.allSeats].indexOf(seat)
         });
-        console.log(this.selectedSeats)
+        localStorage.setItem('selectedSeatsIndex', JSON.stringify(this.seatNumbers));
         this.updateCountAndPrice()
     }
 
     updateCountAndPrice() {
-        this.calculate.count.innerText = this.selectedSeats.length;
-        this.calculate.totalCost.innerText = this.selectedSeats.length * this.ticketPrice
+        this.calculate.count.innerText = this.seatNumbers.length;
+        this.calculate.totalCost.innerText = this.seatNumbers.length * this.ticketPrice;
     }
 
 }
