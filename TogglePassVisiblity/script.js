9235fd3e074a2b4f13018adb407b5d9b9b6dab59
Punit Jain <contactpunitjain@gmail.com>
2020-10-14 20:25:55 +0530

    solved new exercise

--- TogglePassVisiblity/script.js
+++ TogglePassVisiblity/script.js
@@ -1,15 +1,30 @@
-const showPassword = document.getElementById('show-password');
-const password = document.getElementById('password');
 
-showPassword.addEventListener('change', event => {
-    if (password.value) {
-        if (event.target.checked) {
-            password.type='text';
-            console.log('okok')
+// const passwords = document.querySelectorAll('#current-password, #new-password');
+// const toggle = document.querySelector('#show-passwords');
+
+// toggle.addEventListener('click', event => {
+//     if (toggle.checked) {
+//         passwords.forEach(element => {
+//             element.type = 'password';
+//         })
+//     }
+//     else {
+//         passwords.forEach(element => {
+//             element.type = 'text';
+//         })
+//     }
+// })
+
+const passwords = document.querySelectorAll('#current-password, #new-password');
+const toggle = document.querySelector('#show-passwords');
+
+toggle.addEventListener('click', event => {
+    passwords.forEach(element => {
+        if (toggle.checked) {
+            element.type = 'text';
         }
         else {
-            password.type = 'password';
+            element.type = 'password';
         }
-    }
-    
-});
+    })
+})
