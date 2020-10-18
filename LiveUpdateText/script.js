35dc6176a3515dc52f45031fa0a98a24c1ca52ec
Punit Jain <contactpunitjain@gmail.com>
2020-10-18 17:47:38 +0530

    added code to test number of characters types live

--- LiveUpdateText/script.js
+++ LiveUpdateText/script.js
@@ -2,6 +2,5 @@ const textEl = document.querySelector('#text');
 const span = document.querySelector('span');
 
 textEl.addEventListener('input', () => {
-    console.log('hehe')
     span.textContent = textEl.value.length;
 })
