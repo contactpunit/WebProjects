f61dabec1347fbefcc7330b8c2c4023a4cf57768
Punit Jain <contactpunitjain@gmail.com>
2020-10-25 19:52:20 +0530

    added functionality for infinit scrolling and search

--- InfiniteScrolling/script.js
+++ InfiniteScrolling/script.js
@@ -3,9 +3,37 @@ class InfiniteScroll {
         this.postContainer = document.querySelector('#post-container');
         this.loader = document.querySelector('.loader');
         this.filter = document.querySelector('#filter');
-        this.limit = 3;
+        this.limit = 5;
         this.pageNumber = 1;
         this.url = `https://jsonplaceholder.typicode.com/posts?_limit=${this.limit}&_page=${this.pageNumber}`
+        window.addEventListener('scroll', this.fetchNewPosts.bind(this));
+        this.filter.addEventListener('input', this.filterPosts.bind(this));
+    }
+
+    filterPosts(event) {
+        const typedTerm = event.target.value.toLowerCase();
+        const currentPosts = document.querySelectorAll('.post');
+        currentPosts.forEach(post => {
+            const title = post.querySelector('.post-title').innerText.toLowerCase();
+            const body = post.querySelector('.post-body').innerText;
+            if (title.indexOf(typedTerm) > -1 || body.indexOf(typedTerm) > -1) {
+                post.style.display = 'flex';
+            }
+            else {
+                post.style.display = 'none';
+            }
+        })
+    }
+
+    fetchNewPosts() {
+        const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+        if (scrollTop + clientHeight >= scrollHeight - 5) {
+            this.loader.classList.add('show');
+            this.pageNumber += 1;
+            this.url = `https://jsonplaceholder.typicode.com/posts?_limit=${this.limit}&_page=${this.pageNumber}`;
+            this.scroll();
+            this.loader.classList.remove('show');
+        }
     }
 
     async getPosts() {
@@ -19,7 +47,7 @@ class InfiniteScroll {
         }
     }
 
-    async run() {
+    async scroll() {
         const results = await this.getPosts();
         if (results) {
             results.forEach(element => {
@@ -38,4 +66,4 @@ class InfiniteScroll {
 }
 
 const i = new InfiniteScroll();
-i.run()
+i.scroll()
