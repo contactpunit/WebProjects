d1ccb8803994a9f676e1a4fdbe56b103c76499dc
Punit Jain <contactpunitjain@gmail.com>
2020-10-26 18:54:42 +0530

    modified script to account for unique quotes only among last 50 quotes

--- RandomRon/script.js
+++ RandomRon/script.js
@@ -1,14 +1,29 @@
 const quoteEl = document.querySelector('#quote');
 const button = document.querySelector('button');
+const maxLen = 50;
+const oldQuotes = [];
 button.addEventListener('click', getQuote);
 const quoteUrl = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
 function getQuote() {
+    console.log(oldQuotes);
     fetch(quoteUrl)
         .then(response => response.json())
         .then(data => {
             const quote = data[0];
+            if (oldQuotes.indexOf(quote) == -1) {
+                if (oldQuotes.length >= maxLen) {
+                    oldQuotes.shift();
+                }
+                oldQuotes.push(quote);
+            }
+            else {
+                getQuote();
+            }
             quoteEl.textContent = quote;
         })
-        .catch(error => console.log(error))
+        .catch(error => {
+            console.log(error)
+            quoteEl.textContent = 'Something went wrong. Try again !!'
+        })
 }
 getQuote();
