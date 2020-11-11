ea7778f8054ad5f7ffe67b97c346e55fad7ec1d4
Punit Jain <contactpunitjain@gmail.com>
2020-11-11 13:17:58 +0530

    nodelist to array exercise

---
+++ NodeList2Array/script.js
@@ -0,0 +1,11 @@
+const body = document.querySelector('body')
+const divs = document.querySelectorAll('div');
+
+// Traverse each div for subelements
+// divs.forEach(div => {
+//     const nxt = div.nextSibling;
+//     console.log(nxt)
+// })
+
+const elems = getElementsByTagName('h2,h3,h4,h5');
+console.log(elems)
