a941734e9de483986e646581f91babef5a0f530c
Punit Jain <contactpunitjain@gmail.com>
2020-12-05 11:01:27 +0530

    Read data rom cache if not expired

---
+++ APICache/script.js
@@ -0,0 +1,59 @@
+const elem = document.querySelector('.all-articles');
+
+function loadData() {
+    const storageKey = 'newsData';
+    const expiryTime = 1000 * 60;
+    const url = 'https://vanillajsacademy.com/api/pirates.json';
+
+    const cacheData = isDataValid(storageKey, expiryTime);
+    if (cacheData) {
+        renderData(cacheData);
+    }
+    else {
+        const pirateData = fetch(url)
+            .then(res => res.json())
+            .then(data => data.articles)
+            .then((articles) => {
+                updateLocalStorage(articles, storageKey);
+                renderData(articles);
+            })
+            .catch(err => {
+                console.log(`Error in fetching data: ${err}`)
+            })
+    }
+}
+
+// check localstorage if data there
+function isDataValid(storageKey, expiryTime) {
+    const newsData = localStorage.getItem(storageKey);
+    if (!newsData) return false;
+    const data = JSON.parse(newsData);
+    if (!data.data || !data.timestamp) return false;
+    const difference = new Date().getTime() - data.timestamp;
+    if (difference > expiryTime) return false
+    return data.data;
+}
+
+function updateLocalStorage(data, storageKey) {
+    localStorage.setItem([storageKey],
+        JSON.stringify({
+            data,
+            timestamp: new Date().getTime()
+        }))
+}
+
+function renderData(data) {
+    const elements = data.map(element => {
+        return `
+        <div class="article">
+        <h2>${element.title}</h2>
+        <p class="post-body">${element.article}<p>
+        </div>
+    `
+    }).join('');
+    elem.innerHTML = elements
+
+}
+
+loadData()
+
