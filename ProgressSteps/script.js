ec59e062258f57fd922fbab722f48ce9ff321f0b
Punit Jain <contactpunitjain@gmail.com>
2020-12-22 11:28:55 +0530

    added code to adjust progress bar and active class

--- ProgressSteps/script.js
+++ ProgressSteps/script.js
@@ -5,30 +5,34 @@ class ProgressSteps {
         this.next = document.querySelector('#next');
         this.circles = Array.prototype.slice.call(document.querySelectorAll('.circle'));
         this.activeCircle = document.querySelector('.circle.active');
+        this.currentIndex = 0;
         this.next.addEventListener('click', () => {
-            this.currentIndex = this.circles.indexOf(document.querySelector('.circle.active'));
-            if (this.currentIndex + 1 >= this.circles.length) return;
-            this.circles[this.currentIndex].classList.remove('active')
-            this.circles[this.currentIndex + 1].classList.add('active');
-            this.prev.disabled = false;
-            if (this.currentIndex + 2 >= this.circles.length) {
+            if (this.currentIndex + 2 == this.circles.length) {
                 this.currentIndex = this.circles.length - 1;
+                this.circles[this.currentIndex].classList.add('active');
+                this.prev.disabled = false;
                 this.next.disabled = true;
-                return;
             }
+            if (this.currentIndex + 1 >= this.circles.length) {
+                this.progress.style.width = (this.currentIndex) / (this.circles.length - 1) * 100 + '%'
+                this.currentIndex = this.circles.length - 1;
+                return
+            };
             this.currentIndex += 1;
+            this.prev.disabled = false;
+            this.circles[this.currentIndex].classList.add('active');
+            this.progress.style.width = (this.currentIndex) / (this.circles.length - 1) * 100 + '%'
         })
         this.prev.addEventListener('click', () => {
-            this.currentIndex = this.circles.indexOf(document.querySelector('.circle.active'));
-            if (this.currentIndex === 0) return;
-            this.circles[this.currentIndex].classList.remove('active');
-            this.circles[this.currentIndex - 1].classList.add('active');
-            this.next.disabled = false;
-            if (this.currentIndex == 1) {
+            if (this.currentIndex <= 1) {
+                this.progress.style.width = (this.currentIndex) / (this.circles.length - 1) * 100 + '%'
                 this.prev.disabled = true;
-                return
             }
+            this.circles[this.currentIndex].classList.remove('active');
             this.currentIndex -= 1;
+            this.progress.style.width = (this.currentIndex) / (this.circles.length - 1) * 100 + '%'
+            this.next.disabled = false;
+
         })
     }
 
