1ba99fad85ed0d49aa776f7f865b1fa9b220445f
Punit Jain <contactpunitjain@gmail.com>
2021-02-14 18:35:21 +0530

    added code to handle random selction and removal using setTimeout and etInterval

--- RandomPicker/script.js
+++ RandomPicker/script.js
@@ -2,17 +2,43 @@ class RandomPicker {
     constructor() {
         this.textEl = document.querySelector('#textarea');
         this.tagEl = document.querySelector('#tags');
+        this.elems = [];
         this.textEl.addEventListener('keyup', this.populateTags.bind(this));
     }
 
     populateTags(event) {
-        const allTags = event.target.value.split(',');
-        const tags = allTags.map(element => {
-            if (element.trim()) {
-                return `<span class="tag">${element}</span>`
-            }
-        }).join('')
-        this.tagEl.innerHTML = tags;
+        if (event.key === 'Enter') {
+            const allTags = event.target.value.split(',');
+            allTags.forEach(element => {
+                if (element.trim()) {
+                    const t = document.createElement('span');
+                    t.classList.add('tag');
+                    t.textContent = element;
+                    this.tagEl.appendChild(t)
+                    this.elems.push(t)
+                }
+            })
+            this.chooseRandomEl(this.elems)
+        }
+    }
+
+    chooseRandomEl(allTags) {
+        //choose Random element
+        //add highlight class to element
+        // set Timeout to remove after 200ms
+        const highlight = setInterval(() => {
+            const r = allTags[Math.floor(Math.random() * allTags.length)];
+            r.classList.add('highlight')
+            setTimeout(() => { r.classList.remove('highlight') }, 100)
+        }, 200)
+
+        setTimeout(() => {
+            // remove setInterval highlight
+            // choose one element and highlight it
+            clearInterval(highlight);
+            const elem = allTags[Math.floor(Math.random() * allTags.length)];
+            elem.classList.add('highlight')
+        }, 3000)
     }
 }
 
